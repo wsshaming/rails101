52616a1de14417a1f4571a39a7ecdad17e308329
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-01-26 17:02:45 +0800

    implement group relation

--- app/models/group_relationship.rb
+++ app/models/group_relationship.rb
@@ -1,4 +1,5 @@
 class GroupRelationship < ApplicationRecord
-  has_many :group_relationships
-  has_many :members, through: :group_relationships, source: :user
+  belongs_to :group
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
 
          has_many :group_relationships
          has_many :participated_groups, :through => :group_relationships, :source => :group
+
 end
